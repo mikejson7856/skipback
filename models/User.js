ac42a52833de7ccc0b89dddc29caae7cd27326b2
rana <rana.buddy@gmail.com>
2024-08-13 22:26:18 +0600

    feat:first commit

--- models/User.js
+++ models/User.js
@@ -27,7 +27,9 @@ const userSchema = new Schema({
     updated_at:{type: Date},
 
 qrCodeStatus:{ type: Boolean, default: false },
-phone: { type: String}
+phone: { type: String},
+email: {  type: String,
+    trim: true},
 
 
 
